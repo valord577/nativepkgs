a1d2308d267f413c28b2b8ca9f8834c188457cd2
valord577 <valord577@gmail.com>
2025-01-21 17:50:24 +0800

    add libpng16

---
+++ scripts/libpng16.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+set -e
+
+# ----------------------------
+# packages
+# ----------------------------
+source "${PROJ_ROOT}/pkg-conf.sh"
+[ "${PKG_PLATFORM}" != "macosx" ] && \
+  {
+    dl_pkgc zlib-ng  'c939498'   static
+  }
+
+printf "\e[1m\e[35m%s\e[0m\n" "${PKG_CONFIG_PATH}"
+# ----------------------------
+# static or shared
+# ----------------------------
+case ${PKG_TYPE} in
+  "static")
+    PKG_TYPE_FLAG="--enable-shared=no"
+    ;;
+  *)
+    printf "\e[1m\e[31m%s\e[0m\n" "Invalid PKG TYPE: '${PKG_TYPE}'."
+    exit 1
+    ;;
+esac
+# ----------------------------
+# compile :p
+# ----------------------------
+if [ "${CLANGD_CODE_COMPLETION}" == "1" ]; then
+  PKG_BULD_DIR="${PROJ_ROOT}"
+else
+  { rm -rf ${PKG_BULD_DIR}; mkdir -p "${PKG_BULD_DIR}"; }
+  { rm -rf ${PKG_INST_DIR}; mkdir -p "${PKG_INST_DIR}"; }
+fi
+
+CONFIGURE_COMMAND=$(cat <<- EOF
+${SUBPROJ_SRC}/configure     \
+  --prefix='${PKG_INST_DIR}' \
+  ${PKG_TYPE_FLAG}  \
+  ${PKG_BULD_TYPE}  \
+  ${PKG_INST_STRIP} \
+  --enable-pic=yes \
+  --disable-tests  \
+  --disable-tools  \
+  --enable-werror  \
+  --disable-dependency-tracking \
+  --enable-hardware-optimizations=yes \
+  ${PKG_DEPS_ARGS}
+EOF
+)
+
+case ${PKG_PLATFORM} in
+  "macosx" | "iphoneos" | "iphonesimulator")
+    CONFIGURE_COMMAND="${CONFIGURE_COMMAND} --host=${PKG_ARCH}"
+    ;;
+  *)
+    ;;
+esac
+
+printf "\e[1m\e[36m%s\e[0m\n" "${CONFIGURE_COMMAND}"
+pushd -- "${PKG_BULD_DIR}"; eval ${CONFIGURE_COMMAND}; popd
+
+# build & install
+MAKE_COMMAND="make -j ${PARALLEL_JOBS}"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-libLTLIBRARIES"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-pkgconfigDATA"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-nodist_pkgincludeHEADERS"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-pkgincludeHEADERS"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-header-links"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-libpng-pc"
+MAKE_COMMAND="${MAKE_COMMAND}; make install-library-links"
+
+if command -v bear >/dev/null 2>&1 ; then
+  MAKE_COMMAND="bear -- ${MAKE_COMMAND}"
+fi
+printf "\e[1m\e[36m%s\e[0m\n" "${MAKE_COMMAND}"
+pushd -- "${PKG_BULD_DIR}"; eval ${MAKE_COMMAND}; popd
+
+rm -rf ${PKG_INST_DIR}/lib/*.la
---
+++ scripts/wxWidgets.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -e
+
+# ----------------------------
+# packages
+# ----------------------------
+source "${PROJ_ROOT}/pkg-conf.sh"
+dl_pkgc libpng16  'c939498'   static '' '' '' 'PNG'
+
+printf "\e[1m\e[35m%s\e[0m\n" "${PKG_CONFIG_PATH}"
+# ----------------------------
+# static or shared
+# ----------------------------
+case ${PKG_TYPE} in
+  "static")
+    PKG_TYPE_FLAG="-D BUILD_SHARED_LIBS:BOOL=0"
+    ;;
+  *)
+    printf "\e[1m\e[31m%s\e[0m\n" "Invalid PKG TYPE: '${PKG_TYPE}'."
+    exit 1
+    ;;
+esac
+# ----------------------------
+# optimize
+#  - 0 DEBUG
+#  - 1 RELEASE (default)
+# ----------------------------
+LIB_RELEASE=${LIB_RELEASE:-"1"}
+if [ "${LIB_RELEASE}" == "1" ]; then
+  PKG_BULD_TYPE="-D CMAKE_BUILD_TYPE=Release"
+  PKG_INST_STRIP="--strip"
+else
+  PKG_BULD_TYPE="-D CMAKE_BUILD_TYPE=Debug"
+  PKG_INST_STRIP=""
+fi
+# ----------------------------
+# compile :p
+# ----------------------------
+{ rm -rf ${PKG_BULD_DIR}; mkdir -p "${PKG_BULD_DIR}"; }
+{ rm -rf ${PKG_INST_DIR}; mkdir -p "${PKG_INST_DIR}"; }
+
+CMAKE_COMMAND=$(cat <<- EOF
+cmake -S "${SUBPROJ_SRC}" -B "${PKG_BULD_DIR}" \
+  -D CMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON   \
+  -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+  -D CMAKE_INSTALL_PREFIX="${PKG_INST_DIR}"  \
+  -D CMAKE_INSTALL_LIBDIR:PATH=lib \
+  -D CMAKE_PREFIX_PATH="${PKG_DEPS_CMAKE}" \
+  -D CMAKE_FIND_ROOT_PATH="${SYSROOT};${PKG_DEPS_CMAKE}" \
+  ${PKG_BULD_TYPE} ${PKG_TYPE_FLAG} ${CMAKE_EXTRA} \
+  -D wxBUILD_CXX_STANDARD=11 \
+  -D wxBUILD_MONOLITHIC=ON   \
+  -D wxBUILD_PRECOMP=OFF     \
+  -D wxBUILD_USE_STATIC_RUNTIME=ON \
+  -D wxUSE_UNICODE_UTF8=ON  \
+  -D THIRDPARTY_DEFAULT=sys \
+  -D wxUSE_REGEX=OFF   \
+  -D wxUSE_LIBJPEG=OFF \
+  -D wxUSE_LIBTIFF=OFF \
+  -D wxUSE_NANOSVG=OFF
+EOF
+)
+printf "\e[1m\e[36m%s\e[0m\n" "${CMAKE_COMMAND}"; eval ${CMAKE_COMMAND}
+
+# build & install
+cmake --build "${PKG_BULD_DIR}" -j ${PARALLEL_JOBS}
+cmake --install "${PKG_BULD_DIR}" ${PKG_INST_STRIP}
