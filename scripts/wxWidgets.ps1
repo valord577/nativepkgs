f64b0eb8fea2cebc824937e0df06c51538049e50
valord577 <valord577@gmail.com>
2025-02-07 19:58:03 +0800

    add wxWidgets

--- scripts/wxWidgets.ps1
+++ scripts/wxWidgets.ps1
@@ -36,17 +36,19 @@ if ($LIB_RELEASE -eq $null) {
   $LIB_RELEASE = "1"
 }
 if ($LIB_RELEASE -ieq "1") {
+  $_build_config = "Release"
   $PKG_BULD_TYPE = @"
 ``
-  -D CMAKE_BUILD_TYPE=Release ``
+  -D CMAKE_BUILD_TYPE=${_build_config} ``
   -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
 "@
   $PKG_INST_STRIP = "--strip"
 } else {
   <#
+  $_build_config = "Debug"
   $PKG_BULD_TYPE = @"
 ``
-  -D CMAKE_BUILD_TYPE=Debug ``
+  -D CMAKE_BUILD_TYPE=${_build_config} ``
   -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug ``
   -D CMAKE_EXE_LINKER_FLAGS_DEBUG="/debug /INCREMENTAL:NO" ``
   -D CMAKE_SHARED_LINKER_FLAGS_DEBUG="/debug /INCREMENTAL:NO" ``
@@ -76,6 +78,7 @@ cmake -G Ninja ``
   -D CMAKE_PREFIX_PATH="${PKG_DEPS_CMAKE}" ``
   -D CMAKE_FIND_ROOT_PATH="${SYSROOT};${PKG_DEPS_CMAKE}" ``
   ${PKG_BULD_TYPE} ${PKG_TYPE_FLAG} ${CMAKE_EXTRA} ``
+  -D CMAKE_CONFIGURATION_TYPES="${_build_config}" ``
   -D wxBUILD_CXX_STANDARD=11 ``
   -D wxBUILD_MONOLITHIC=ON   ``
   -D wxBUILD_USE_STATIC_RUNTIME=ON ``
@@ -93,7 +96,7 @@ Invoke-Expression -Command "${CMAKE_COMMAND}"
 if (($LASTEXITCODE -ne $null) -and ($LASTEXITCODE -ne 0)) { exit $LASTEXITCODE }
 
 # build & install
-cmake --build "${PKG_BULD_DIR}" -j ${PARALLEL_JOBS}
+cmake --build "${PKG_BULD_DIR}" -j ${PARALLEL_JOBS} --config ${_build_config}
 if (($LASTEXITCODE -ne $null) -and ($LASTEXITCODE -ne 0)) { exit $LASTEXITCODE }
 
 cmake --install "${PKG_BULD_DIR}" ${PKG_INST_STRIP}
