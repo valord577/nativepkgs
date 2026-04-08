b92e3cc6dd7c665fe79d73c9b8eb1ed76e8d0565
valord577 <valord577@gmail.com>
2026-04-08 16:49:07 +0800

    Refactor: Modular Programming in Python

--- scripts/utils.py
+++ scripts/utils.py
@@ -56,7 +56,7 @@ def _util_load_pyfile(file: str, attrs: Union[list[str], None] = None):
     return module
 
 def _util_func__exec_python(args: list[str]):
-    _util_func__subprocess([(Path(sys.executable)).as_posix(), *args])
+    _util_func__subprocess([(Path(sys.executable)).absolute().as_posix(), *args])
 def _util_func__pip_install(packages: list[str]):
     args = ['-m', 'pip', 'install', '--upgrade']
     if not ON_GITHUB_CI:
