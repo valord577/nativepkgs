8a37922bf3d61fb3b327f9568e6ce477b522b319
valord577 <valord577@gmail.com>
2026-04-17 12:00:47 +0800

    module: update mbedtls4 (#80)

--- scripts/utils.py
+++ scripts/utils.py
@@ -72,15 +72,15 @@ def _util_load_pyfile(file: str, attrs: Union[list[str], None] = None):
         getattr(module, attr)
     return module
 
-def _util_func__exec_python(args: list[str]):
-    _util_func__subprocess([(Path(sys.executable)).absolute().as_posix(), *args])
+def _util_func__exec_python(args: list[str], cwd: Union[str, None] = None):
+    _util_func__subprocess(args=[(Path(sys.executable)).absolute().as_posix(), *args], cwd=cwd)
 def _util_func__pip_install(packages: list[str]):
     args = ['-m', 'pip', 'install', '--upgrade']
     if not ON_GITHUB_CI:
         args.extend(['--trusted-host', 'repo.huaweicloud.com'])
         args.extend(['-i', 'https://repo.huaweicloud.com/repository/pypi/simple'])
     args.extend(['pip', *packages])
-    _util_func__exec_python(args)
+    _util_func__exec_python(args=args)
 
 def _util_put_pkg_version_desc(pkg_name: str, ver: str):
     (Path(PROJ_ROOT) / '.deps' / f'{pkg_name}.ver') \
